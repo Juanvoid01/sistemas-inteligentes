dae13e01b588585d409e35d711196e24b0d3e7ac
jaime <jaimes10@ucm.es>
2025-03-13 16:33:14 +0100

    Añadido pdf, variables con datos de perception, estado rompiendo y esquivando

--- PythonGym/BaseAgent.py
+++ PythonGym/BaseAgent.py
@@ -22,6 +22,8 @@ class BaseAgent:
     class State(Enum):
         EXPLORANDO = auto()
         ATACANDO = auto()
+        ROMPIENDO = auto()
+        ESQUIVANDO = auto()
 
     state = State.EXPLORANDO
 
@@ -45,7 +47,16 @@ class BaseAgent:
     #Devuelve la acción u el disparo si o no
     def Update(self, perception):
         print("Toma de decisiones del agente")
-        print(perception)    
+        print(perception)
+        
+        vista_up, dist_up = perception[0], perception[4]
+        vista_down, dist_down = perception[1], perception[5]
+        vista_right, dist_right = perception[2], perception[6]
+        vista_left, dist_left = perception[3], perception[7]
+        player_x, player_y = perception[8], perception[9]
+        command_x, command_y = perception[10],  perception[11]
+
+
 
         self.analizar(perception)
 
