1fd6e03dbcf3b30182f518c2261bc8c5a676ab20
Paul Harrison <pharrison31415@gmail.com>
2025-05-07 14:54:20 +0200

    add mae to random forest

--- random-forest-regressor.py
+++ random-forest-regressor.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import svm
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 
 # Load dataset
@@ -44,3 +44,4 @@ y_pred = rf.predict(X_test)
 # Evaluate the tuned model
 print("Random Forest RMSE:", mean_squared_error(y_test, y_pred))
 print("Random Forest R2 Score:", r2_score(y_test, y_pred))
+print("Random Forest MAE:", mean_absolute_error(y_test, y_pred))
